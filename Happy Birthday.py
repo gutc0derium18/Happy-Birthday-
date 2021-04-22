57d34a39b25297b1eb14b1a0e8b2068f84797e24
gutc0derium18 <82983276+gutc0derium18@users.noreply.github.com>
2021-04-21 20:26:38 -0500

    Happy_Birthday.py

---
+++ Happy Birthday.py
@@ -0,0 +1,8 @@
+from playsound import playsound
+playsound('audio.mp3')
+
+print(" Would you like me to play Happy Birthday?")
+answer = input("Answer Here:").lower()
+  if answer ('yes', 'sure', 'of course')
+    print("Great!")
+    answer1 = input("Just one question, which music genre do you like best?")
